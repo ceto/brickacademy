685c770601f1530d9f9b81fefa680e2147617deb
Gabor Szabo <szabogabi@gmail.com>
2019-03-31 16:32:31 +0200

    slider added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,5 @@
-import $ from 'jquery';
-import 'what-input';
+import $ from "jquery";
+import "what-input";
 
 // Foundation JS relies on a global varaible. In ES6, all imports are hoisted
 // to the top of the file so if we used`import` to import Foundation,
@@ -7,14 +7,14 @@ import 'what-input';
 // This is why we have to use CommonJS require() here since it doesn't
 // have the hoisting behavior.
 window.jQuery = $;
-require('foundation-sites');
+require("foundation-sites");
 
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-
 $(document).foundation();
+require("owl.carousel");
 
 $(".membercard").on("click", "a", function(e) {
     e.preventDefault();
@@ -28,4 +28,29 @@ $(".membercard__details").on("click", function(e) {
     $(this)
         .closest(".membercard")
         .removeClass("details-is-shown");
+});
+
+$(document).ready(function() {
+    var tabletbp = Foundation.MediaQuery.get("tablet"),
+        xlargebp = Foundation.MediaQuery.get("xlarge");
+
+    $(".owl-slider").owlCarousel({
+        autoWidth: true,
+        loop: true,
+        nav: true,
+        navText: ["‹", "›"],
+        // navContainer: "#slidernav",
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            tabletbp: {
+                items: 2
+            },
+            xlargebp: {
+                items: 3
+            }
+        }
+    });
 });
